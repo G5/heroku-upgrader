e7544aeb39220446ef932f14575c339c49a73ede
Randy Leighton <randyleighton@yahoo.com>
2016-03-28 14:50:33 -0700

    clean up menus and app module

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,16 @@
 module App
   def get_app_name
-    print "App Name: "
+    print "Enter App Name: "
     @app_name = gets.chomp.to_s
+    clear_screen
   end
+
+  def pg_info
+    puts "heroku pg info for app".cyan
+  end
+
+  def pg_backups_schedule
+   puts "heroku pg:backups schedules for app".cyan
+  end
+ 
 end
--- lib/upgrade.rb
+++ lib/upgrade.rb
@@ -1,27 +1,49 @@
 Dir[File.join(Dir.pwd) + "**/*.rb"].each { |file| require file if !file.include?("upgrade.rb") }
 include App
+require 'pry'
 
 def main_menu
-  system("clear")
-  status=""; choice = ""
+  choice = ""
   until choice == "x"
-    puts "[= Heroku Database Upgrader =]".blue.bold
-    status != "" ? puts("Status: ".magenta + "#{status}"+ "\n\n")  : false
-    puts "(1) Enter App Name "
-    puts "(2) App PG Info"
+    puts "[= Heroku PG Upgrader Main Menu =]".blue.bold
+    @app_name.nil? ? (puts "(1) Enter App Name: ") : (puts "(1) Enter App Name (currently:#{@app_name})")
+    puts "(2) [= Database Upgrade Menu =]"
     puts "(x) Exit "
     print "Enter choice: "
     choice = gets.chomp.to_s
     case choice
     when "1"
       get_app_name
-      status = "Getting App Name #{@app_name}"
-    else
-      status = "Invalid Option"
+    when "2"
+      database_upgrade_menu
     end
   end
   puts "Closing program"
 end
 
+def database_upgrade_menu
+  choice = ""; clear_screen
+  until choice == "x"
+    puts "[= Database Upgrade Menu =]".blue.bold
+    puts "(1) PG Info "
+    puts "(2) pg:backups schedules "
+    puts "(x) Exit "
+    print "Enter choice: "
+    choice = gets.chomp.to_s
+    case choice
+    when "1"
+      pg_info
+    when "2"
+      pg_backups_schedule
+    end
+  end
+  clear_screen
+end
 
+
+def clear_screen
+  system("clear")
+end
+
+clear_screen
 main_menu
