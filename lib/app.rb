fcd30f71e208bc20bffbb32964a8e295d597d4f2
Randy Leighton <randyleighton@yahoo.com>
2016-03-28 16:03:17 -0700

    app name now joins app or resets if it fails

--- lib/app.rb
+++ lib/app.rb
@@ -10,8 +10,9 @@ module App
     join_url = "heroku join -a #{@app_name}"
     pg_info, stderr, status = Bundler.with_clean_env {Open3.capture3(join_url)}
     if status.success?
-      puts "Joined #{@app_name} successfully"
+      puts "Joined #{@app_name} successfully".green
     else
+      @app_name = nil
       puts "Error: ".red + "#{stderr}"
     end
   end
@@ -20,8 +21,8 @@ module App
     get_pg_url = "heroku pg:info -a #{@app_name}"
     pg_info, stderr, status = Bundler.with_clean_env {Open3.capture3(get_pg_url)}
     if status.success?
-      puts "-=[Heroku pg info for #{@app_name}".cyan
-      puts pg_info 
+      puts "-=[Heroku pg info for " + "#{@app_name}".cyan + "]=-"
+      puts pg_info.green
     else
       puts "Error: ".red + "#{stderr}"
     end
