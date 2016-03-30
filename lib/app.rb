ba5f38cd26a37e3fd88ec6e62e1ea3e0ba4e926d
Randy Leighton <randyleighton@yahoo.com>
2016-03-30 13:49:50 -0700

    menu updated and now creates addon to upgrade and correctly displays options

--- lib/app.rb
+++ lib/app.rb
@@ -39,24 +39,11 @@ module App
     end
   end
 
-  def create_addon_choice
-    addon_choice = ""
-    puts "This creates the addon the current database will copy into and then this will get promoted:"
-    puts "Upgrade to " + "(B)".green + "asic $9/mo" + " OR " + "(S)".red + "tandard $50/mo"
-    addon_choice = gets.chomp.to_s.downcase
-    if addon_choice == "b"
-      addon_url = "heroku addons:create heroku-postgresql:hobby-dev -a #{@app_name}"
-      create_addon(addon_url)
-    elsif addon_choice == "s"
-      addon_url = "heroku addons:create heroku-postgresql:hobby-dev -a #{@app_name}"
-      create_addon(addon_url)
-    else
-      puts "Canceling create addon, invalid choice."
-    end
-  end
-
   def create_addon(addon_url)
-    puts addon_url
+    addon_create, stderr, status = Bundler.with_clean_env {Open3.capture3(addon_url)}
+    puts addon_create
+    @addon_color = addon_create.slice(/HEROKU\w+/)
+    binding.pry
   end
 
 end
--- lib/upgrade.rb
+++ lib/upgrade.rb
@@ -4,15 +4,14 @@ require 'pry'
 require 'dotenv'
 require 'open3'
 require 'Bundler'
-
 Dotenv.load
 
 def main_menu
   choice = ""
   until choice == "x"
     puts "[= Heroku PG Upgrader Main Menu =]".blue.bold
-    @app_name.nil? ? (puts "(1) Enter App Name: ") : (puts "(1) Enter App Name (currently:" + "#{@app_name})".cyan)
-    puts "(2) [= Database Upgrade Menu =]"
+    @app_name.nil? ? (puts "(1) enter heroku appname: ") : (puts "(1) change heroku appname " + "( " + "#{@app_name}".cyan + " )" )
+    puts "(2) [= database Upgrade Menu =]"
     puts "(x) Exit "
     print "Enter choice: "
     choice = gets.chomp.to_s
@@ -30,24 +29,48 @@ def database_upgrade_menu
   choice = ""; clear_screen
   until choice == "x"
     puts "[= Database Upgrade Menu =] ".blue.bold + "#{@app_name}".cyan 
-    puts "(1) pg:info"
-    puts "(2) pg:backups schedules"
-    puts "(3) create the addon"
+    puts "(1) pg:info & pg:backups schedules"
+    if @addon_color
+      puts "(3) copy/promote " + "#{@addon_color}".cyan
+    else
+      puts "(2) create addon"
+    end
     puts "(x) Exit "
     print "Enter choice: "
     choice = gets.chomp.to_s
     case choice
     when "1"
       pg_info
-    when "2"
       pg_backups_schedule
-    when "3"
+    when "2"
       create_addon_choice
+    when "3"
+      copy_promote_addon
     end
   end
   clear_screen
 end
 
+
+def create_addon_choice
+    choice = ""
+    puts "This creates the addon the current database will copy into and then this will get promoted:"
+    puts "Upgrade to " + "(B)".green + "asic $9/mo" + " OR " + "(S)".red + "tandard $50/mo"
+    choice = gets.chomp.to_s.downcase
+    if choice == "b"
+      addon_url = "heroku addons:create heroku-postgresql:hobby-basic -a #{@app_name}"
+      create_addon(addon_url)
+    elsif choice == "s"
+      addon_url = "heroku addons:create heroku-postgresql:hobby-dev -a #{@app_name}"
+      create_addon(addon_url)
+    else
+      puts "Canceling create addon, invalid choice."
+    end
+end
+
+def copy_promote_addon
+  puts "copy promote addon!"
+end
 
 def clear_screen
   system("clear")
