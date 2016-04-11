2141a9afb69598f851a86661f83284c20b219bd2
Randy Leighton <randyleighton@yahoo.com>
2016-04-11 11:51:39 -0700

    test for more states

--- spec/state_spec.rb
+++ spec/state_spec.rb
@@ -16,4 +16,22 @@ describe State do
     expect(@new_state.joined).to eq true
   end
 
+  it "allows the state.addon_created value to be true" do
+    @new_state.addon_created = true    
+    expect(@new_state.addon_created).to eq true
+  end
+  it "allows the state.addon_copied value to be true" do
+    @new_state.addon_copied = true    
+    expect(@new_state.addon_copied).to eq true
+  end
+  it "allows the state.addon_promoted value to be true" do
+    @new_state.addon_promoted = true    
+    expect(@new_state.addon_promoted).to eq true
+  end
+
+  it "allows the state.finalized value to be true" do
+    @new_state.finalized = true    
+    expect(@new_state.finalized).to eq true
+  end
+
 end
