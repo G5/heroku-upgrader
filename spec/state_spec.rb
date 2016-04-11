5266821d9a92df2825b5d300386723206ea7913a
Randy Leighton <randyleighton@yahoo.com>
2016-04-11 11:17:40 -0700

    test for state and state.joined

--- spec/state_spec.rb
+++ spec/state_spec.rb
@@ -1,1 +1,19 @@
 require 'rspec'
+require 'state'
+
+describe State do
+
+  before do
+    @new_state = State.new
+  end
+
+  it "initializes an instance of State" do
+    expect(@new_state).to be_an_instance_of State
+  end
+
+  it "allows the state.joined value to be true" do
+    @new_state.joined = true    
+    expect(@new_state.joined).to eq true
+  end
+
+end
